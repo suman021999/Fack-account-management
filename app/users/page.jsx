7e032423aef5a123b8538c705312c6a60567421e
Shank34 <spikbang@gmail.com>
2024-03-21 13:25:28 +0530

    description

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+
 import AccordionUI from '@/components/AccordionUI'
 const Users = () => {
   return (
     <>
      <div className='flex flex-col justify-center items-center mt-10'>
-       <AccordionUI/>
+       <AccordionUI />
       </div>
+
+      <div className='flex flex-col justify-center items-center mt-10'>
+       <Link href='/'><button className='bg-[#eb547c] px-4 py-3 text-2xl font-bold rounded-md'>Main page</button></Link>
+      </div>
+      
     </>
    
   )
